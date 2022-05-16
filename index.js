e33652964dc21a271c68ec4b0c27ac7390b0499f
John Garbero <john.garbero.itb@gmail.com>
2022-05-16 18:49:52 -0500

    passed most tests

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);  
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 5);
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers])
+
+function createFareMultiplier(x) {
+    return function(y) {
+        return x*y
+    }
+}
+
+const multi = createFareMultiplier();
+
+const fareDoubler = function(x) {
+    return multi*2;
+}
+
+const fareTripler = function(x) {
+    return createFareMultiplier(3)
+}
+// const fareDoubler = function(x) {
+//     return createFareMultiplier()*2;
+// }
